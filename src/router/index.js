765b5a6d6c9697a878110a36acf12e76119924c1
lydiah2015 <mitchellemakini15@gmail.com>
2022-09-11 12:02:51 +0300

    worked on login page

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginVue from '../views/LoginVue.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+
+  {
+    path:'/login',
+    name:'login',
+    component: LoginVue
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/LoginVue.vue
@@ -0,0 +1,25 @@
+<template>
+ <form>
+     <div>
+        <input type='text' placeholder="username" v-model="auth.phone_number"/>
+     </div>
+
+     <div>
+         <input type='password' placeholder='password' v-model="auth.password"/>
+     </div>
+
+ </form>
+    
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+
+const auth= reactive ({
+    phone_number: "",
+    password:""
+})
+function onSubmit(){
+    
+}
+</script>
